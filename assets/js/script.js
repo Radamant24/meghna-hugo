cbe093995c170cb5b27e1b5cd5a4637b529f00b8
Radamant24 <radamant24@gmail.com>
2021-01-03 17:50:08 -0300

    toast test

--- assets/js/script.js
+++ assets/js/script.js
@@ -136,13 +136,14 @@ jQuery(function ($) {
 
 async function submitContact(name, email,subject, message){
 
-		Toastify({
-		  text: "Hello!",
-		  gravity: "top",
-		  position: 'center',
-		  backgroundColor: "#0f3443",
-		}).showToast();
+	var myToast = Toastify({
+		text: "This is a toast message",
+		duration: 5000
+	   })
 
+	myToast.showToast();
+
+	
 	if(!name || !email || !subject || !message || grecaptcha.getResponse().length == 0 )
 		return false
 	const response = await fetch("https://douulhx414.execute-api.us-east-1.amazonaws.com/dev/contact-site", {
